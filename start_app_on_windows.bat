8fe593429bc61106ffb301da3c95dcc574a3a069
Leon <leonaviev@gmail.com>
2025-02-02 11:54:59 +0800

    Improve index page UI

--- start_app_on_windows.bat
+++ start_app_on_windows.bat
@@ -2,10 +2,10 @@
 echo Starting Flask Server...
 start "Flask Server" python app.py
 
-rem Optionally wait a few seconds to ensure the Flask server starts up
-timeout /t 5 /nobreak >nul
+@REM rem Optionally wait a few seconds to ensure the Flask server starts up
+@REM timeout /t 5 /nobreak >nul
 
-echo Starting ngrok tunnel...
-start "ngrok" ngrok http --url=merely-big-falcon.ngrok-free.app 5000
+@REM echo Starting ngrok tunnel...
+@REM start "ngrok" ngrok http --url=merely-big-falcon.ngrok-free.app 5000
 
 pause
